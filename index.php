bd8692e9a69c907c8686b9c87bf60388c92f4042
Benjamin Goodacre <f1@goodacre.name>
2023-07-19 00:50:11 +0000

    Show CF-Connecting-IP

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-print "Remote address: " . $_SERVER['REMOTE_ADDR'] . " X Forwarded for: " . $_SERVER['HTTP_X_FORWARDED_FOR'] . "\n";
+print "Remote address: " . $_SERVER['REMOTE_ADDR'] . " X Forwarded for: " . $_SERVER['HTTP_X_FORWARDED_FOR'] . " CF-Connecting-IP: " . $_SERVER['HTTP_CF_CONNECTING_IP'] . "\n";
 ?>
